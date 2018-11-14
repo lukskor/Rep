fe83c270cb91dc5215a0b4124237b2ed8cece59c
lukskor <lukaszskorupinski2001@gmail.com>
2018-11-14 16:22:34 +0100

    nopeXD
    :0
    
    :)

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+/*
+	smiec
+uhkuiltg77i9
+
+
+joiuohu
+pouoyoiuto
+hoiu
+nom
+*/
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
